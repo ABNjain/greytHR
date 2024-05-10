23200ef4ea34e8f3d89cc31178ac35a01a5feeb0
abnjain <abnjain25@gmail.com>
2024-05-11 03:22:10 +0530

    running both backend and frontend with single command

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res)=>{
     res.send("Hello to the API");
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`)

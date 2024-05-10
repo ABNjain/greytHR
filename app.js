9c7ad59b21075b152d4a5eeaa9cedc56a7fc0e3a
abnjain <abnjain25@gmail.com>
2024-05-11 01:49:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res)=>{
+    res.send("Hello to the API");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+});

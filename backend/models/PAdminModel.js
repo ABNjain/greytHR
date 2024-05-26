d30d0cba1e2253a5c46d856707a214d806417e99
abnjain <abnjain25@gmail.com>
2024-05-27 04:01:01 +0530

    admin firstname lastname

--- backend/models/PAdminModel.js
+++ backend/models/PAdminModel.js
@@ -7,6 +7,14 @@ const PAdminSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,

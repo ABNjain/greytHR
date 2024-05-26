a34c94d27d4e6e74b60b35da0756488122c24581
Amit Kumar Dhurway <125995584+AmitDhurway@users.noreply.github.com>
2024-05-27 00:03:09 +0530

    add logn and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React from 'react';
-import WelcomePage from './components/WelcomePage.jsx';
+// import WelcomePage from './components/WelcomePage.jsx';
+// import LoginPage from './components/LoginPage.jsx';
+import RegisterPage from './components/RegisterPage.jsx';
+// import HomePage from './components/HomePage.jsx';
 
 function App() {
   return (
     <>
     <div>
-    <WelcomePage />
+    {/* <WelcomePage /> */}
+    {/* <LoginPage /> */}
+    <RegisterPage />
+    {/* <HomePage /> */}
     </div>
       
     </>

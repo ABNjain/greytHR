ffe41688379c31169df192d1514960b072f8f5cc
abnjain <abnjain25@gmail.com>
2024-05-28 02:14:45 +0530

    URI confing in env

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ require("./config/mongoDB");
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000', // or an array of allowed origins
+    origin: `${process.env.FRONTEND_URI}`, // or an array of allowed origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // if you need to include cookies in the requests
